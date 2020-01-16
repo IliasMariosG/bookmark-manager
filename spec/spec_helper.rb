272771eccaf7c10f09f7d302ddfa1963a148ad3e
Ilias Grigoropoulos <ilias_grigorop2@hotmail.com>
2020-01-16 14:12:56 +0000

    Define my routine within a method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,11 +25,14 @@ require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require_relative './setup_test_database'
 
 # Tell Capybara to talk to BookmarkManager
 Capybara.app = BookmarkManager
 
 RSpec.configure do |config|
+  config.before(:each) do setup_test_database
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
