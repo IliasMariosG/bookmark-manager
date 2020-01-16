3174c40f6c580be41a9341c5700d96b5861fec1b
Ilias Grigoropoulos <ilias_grigorop2@hotmail.com>
2020-01-16 16:46:33 +0000

    Link the url to the title

--- spec/features/create_bookmarks_spec.rb
+++ spec/features/create_bookmarks_spec.rb
@@ -4,7 +4,6 @@ feature 'Creates bookmarks' do
     fill_in('create_url', with: 'https://test.com')
     fill_in('create_title', with: 'Test')
     click_button('Add new')
-    expect(page).to have_content 'https://test.com'
     expect(page).to have_content 'Test'
   end
 end
